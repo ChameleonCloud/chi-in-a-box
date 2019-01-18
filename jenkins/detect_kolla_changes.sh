0d055a8e6fa2ef81c4bab59d1ada96b3e736f377
Jason <jasonanderson@uchicago.edu>
2019-01-18 11:45:50 -0600

    Playing with generic webhook trigger

---
+++ jenkins/detect_kolla_changes.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+#
+# Detect if any files match Kolla services. Just looks at a list of files
+# and sees if they contain a Kolla service name. This is useful for knowing
+# if a set of changed files in a commit affect a Kolla service configuration
+# and therefor should be deployed.
+#
+
+kolla_services=(
+  blazar
+  glance
+  heat
+  horizon
+  ironic
+  keystone
+  neutron
+  nova
+)
+
+detected_services="$(for s in ${kolla_services[@]}; do
+  for l in $@; do
+    if [[ "$l" == *"$s"* ]]; then
+      echo "$s"
+    fi
+  done
+done | sort | uniq)"
+
+echo "$detected_services"
