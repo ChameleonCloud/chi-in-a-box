d4a1cf3cf851489072ab0418548e6819316cc2d0
Jason Anderson <jasonanderson@uchicago.edu>
2020-04-17 18:15:30 -0500

    [keycloak] Fixup export server staying alive
    Turns out, there is no way to just tell Keycloak to export its realm and
    then quit. We have to do this completely crazy thing where we start the
    process, then examine its printed output to know when the export is
    done, and then kill the process (which we can get the pid from by
    setting JBOSS_PIDFILE, thankfully.)

--- roles/keycloak/files/keycloak_backup_realm.sh
+++ roles/keycloak/files/keycloak_backup_realm.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
-set -e -u -o pipefail
-
 realm_name="$1"
 backup_dir="/backup/${realm_name}_$(date +%Y-%m-%dT%H-%m)"
 
@@ -10,13 +8,22 @@ docker exec -uroot keycloak_server chown -R jboss: /backup
 # Create backup directory
 docker exec keycloak_server mkdir -p "$backup_dir"
 
-# Export realm
-# The 'sed' will cause the server to automatically terminate if the message
-# signifying the export completed successfully is printed.
-docker exec keycloak_server /opt/jboss/keycloak/bin/standalone.sh \
+# Make FIFO pipe to read stdout from backgrounded Keycloak process
+fifo=/tmp/krb.$(date +%s).fifo
+mkfifo $fifo
+trap "rm $fifo" EXIT
+
+docker exec -e JBOSS_PIDFILE=/tmp/rb.pid keycloak_server /opt/jboss/keycloak/bin/standalone.sh \
   -Djboss.socket.binding.port-offset=100 -Dkeycloak.migration.action=export \
-  -Dkeycloak.migration.realmName="$realm_name" \
+  -Dkeycloak.migration.realmName=$realm_name \
   -Dkeycloak.migration.usersExportStrategy=DIFFERENT_FILES \
   -Dkeycloak.migration.provider=dir \
-  -Dkeycloak.migration.dir="$backup_dir" \
-  | sed '/KC-SERVICES0035/q'
+  -Dkeycloak.migration.dir=$backup_dir >$fifo &
+
+sed <$fifo '/KC-SERVICES0035/q' && {
+  pid=$(docker exec keycloak_server sh -c 'cat /tmp/rb.pid; rm /tmp/rb.pid')
+  echo "Attempting to kill Keycloak server (pid=$pid)"
+  docker exec keycloak_server sh -c "kill $pid"
+}
+
+wait
