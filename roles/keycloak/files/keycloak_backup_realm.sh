f116c0091fa3715a36476cf2fb961505c297731f
Jason Anderson <jasonanderson@uchicago.edu>
2020-04-17 16:08:28 -0500

    [keycloak] Fix permission issues on backup
    - Ensure the backup directory exists
    - and is owned by the jboss user inside the container.

--- roles/keycloak/files/keycloak_backup_realm.sh
+++ roles/keycloak/files/keycloak_backup_realm.sh
@@ -2,10 +2,18 @@
 set -e -u -o pipefail
 
 realm_name="$1"
+backup_dir="/backup/${realm_name}_$(date +%Y-%m-%dT%H-%m)"
 
-docker exec -it keycloak_server /opt/jboss/keycloak/bin/standalone.sh
-  -Djboss.socket.binding.port-offset=100 -Dkeycloak.migration.action=export
-  -Dkeycloak.migration.realmName="$realm_name"
-  -Dkeycloak.migration.usersExportStrategy=DIFFERENT_FILES
-  -Dkeycloak.migration.provider=dir
-  -Dkeycloak.migration.dir="/backup/${realm_name}_$(date +%Y-%m-%dT%H-%m)"
+# Ensure proper permissions
+docker exec -it -uroot keycloak_server chown -R jboss: /backup
+
+# Create backup directory
+docker exec -it keycloak_server mkdir -p "$backup_dir"
+
+# Export realm
+docker exec -it keycloak_server /opt/jboss/keycloak/bin/standalone.sh \
+  -Djboss.socket.binding.port-offset=100 -Dkeycloak.migration.action=export \
+  -Dkeycloak.migration.realmName="$realm_name" \
+  -Dkeycloak.migration.usersExportStrategy=DIFFERENT_FILES \
+  -Dkeycloak.migration.provider=dir \
+  -Dkeycloak.migration.dir="$backup_dir"
