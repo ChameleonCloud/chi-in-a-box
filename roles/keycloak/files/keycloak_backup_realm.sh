e698474925a2a71844043202c23439b4df2e9324
Jason Anderson <jasonanderson@uchicago.edu>
2020-04-17 16:24:17 -0500

    [keycloak] Ensure server stops after export
    This is not something you can configure, somehow. We hack in a solution
    using the 'sed' utility.

--- roles/keycloak/files/keycloak_backup_realm.sh
+++ roles/keycloak/files/keycloak_backup_realm.sh
@@ -11,9 +11,12 @@ docker exec -it -uroot keycloak_server chown -R jboss: /backup
 docker exec -it keycloak_server mkdir -p "$backup_dir"
 
 # Export realm
+# The 'sed' will cause the server to automatically terminate if the message
+# signifying the export completed successfully is printed.
 docker exec -it keycloak_server /opt/jboss/keycloak/bin/standalone.sh \
   -Djboss.socket.binding.port-offset=100 -Dkeycloak.migration.action=export \
   -Dkeycloak.migration.realmName="$realm_name" \
   -Dkeycloak.migration.usersExportStrategy=DIFFERENT_FILES \
   -Dkeycloak.migration.provider=dir \
-  -Dkeycloak.migration.dir="$backup_dir"
+  -Dkeycloak.migration.dir="$backup_dir" \
+  | sed '/KC-SERVICES0035/q'
