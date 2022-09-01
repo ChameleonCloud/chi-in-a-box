27e60b5706e7229830320b6faf6bf8d7cfe979bf
Michael Sherman <shermanm@uchicago.edu>
2022-09-01 14:28:34 -0500

    add playbook to unstick doni workers in the DB

--- scripts/mysql.sh
+++ scripts/mysql.sh
@@ -7,5 +7,13 @@
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
 pushd $(realpath $DIR/..) >/dev/null
 
-root_pw="$(./cc-ansible view_passwords | grep -E '^database_password' | cut -d' ' -f2)"
-docker exec -it -e MYSQL_PWD="$root_pw" mariadb mysql -uroot "$@"
+RW_USER="root"
+RW_PASSWORD_KEY="database_password"
+
+get_pw_cmd="./cc-ansible view_passwords"
+root_pw=`$get_pw_cmd | grep -E ^$RW_PASSWORD_KEY: | cut -d' ' -f2`
+
+docker exec -it \
+    -e MYSQL_PWD="$root_pw" \
+    mariadb \
+    mysql -uroot "$@"
